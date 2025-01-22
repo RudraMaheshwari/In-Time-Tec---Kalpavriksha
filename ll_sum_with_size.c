0ae659a55065e2833ee7799a376b9838c375487d
Rudransh Maheshwari <maheshwarirudransh79@gmail.com>
2025-01-22 17:36:57 +0530

    Updated_naming_conventions
    Updated all the naming conventions in the code.

--- ll_sum_with_size.c
+++ ll_sum_with_size.c
@@ -14,45 +14,53 @@ struct node *create_node(int value){
     return new_node;
 }
 
-void add_to_list(int value,int *count,int window,int *sum){
+void add_to_list(int value,int *count_nodes,int window,int *sum){
     struct node *new_node = create_node(value);
     *sum = *sum + value;
-    if(*count < window){
+    if(*count_nodes < window){
         new_node->next = head;
         head =new_node;
-        (*count)++;
+        (*count_nodes)++;
     }
     else if(window == 0){
         *sum =0;
         return;
     }
     else{
-        struct node *temp = head;
-        struct node *prev = NULL;
-        while(temp->next != NULL){
-            prev = temp;
-            temp = temp->next;
+        struct node *temporary = head;
+        struct node *previous = NULL;
+        while(temporary->next != NULL){
+            previous = temporary;
+            temporary = temporary->next;
         }
-        *sum = *sum - (temp->data);
-        free(temp);
-        if (prev != NULL) {
-            prev->next = NULL;
+        *sum = *sum - (temporary->data);
+        free(temporary);
+        if (previous != NULL) {
+            previous->next = NULL;
         }
         new_node->next = head;
         head = new_node;
     }
 }
 
+void display(){
+    struct node *temporary = head;
+    while(temporary != NULL){
+        printf("%d ",temporary->data);
+        temporary = temporary->next;
+    }
+}
+
 int main()
 {
-    int n,window_size,array[100];
+    int operations,window_size,result_array[100];
     printf("Enter No of operations:\n");
-    scanf("%d",&n);
+    scanf("%d",&operations);
     printf("Enter window size:\n");
     scanf("%d",&window_size);
-    int sum = 0, count = 0, count_sum=0;
+    int sum = 0, count_nodes = 0, count_sum=0;
 
-    for(int index = 0; index < n; index++) {
+    for(int index = 0; index < operations; index++) {
         char character;
         int data_value;
         printf("Enter choice and value:\n");
@@ -60,16 +68,14 @@ int main()
 
         if(character == 'A' || character=='a') {
             scanf("%d", &data_value);
-            add_to_list(data_value,&count,window_size,&sum);
+            add_to_list(data_value,&count_nodes,window_size,&sum);
             printf("Value Added.\n");
         } else if(character == 'S' || character =='s') {
-            array[count_sum++]=sum;
+            result_array[count_sum++]=sum;
         }
     }
     for(int index=0; index < count_sum; index++){
-        printf("Sum is: %d\n",array[index]);
+        printf("Sum is: %d\n",result_array[index]);
     }
     return 0;
 }
-
-
